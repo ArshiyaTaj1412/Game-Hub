ed32cdf76886930b4b5c581ce27ef1596a045a8f
LAPTOP-FH4C51QM\arshi <arshiyataj1412@gmail.com>
2025-07-15 21:16:10 +0530

    Added critic scores

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,23 @@
+import { Badge, Button } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "red";
+  return (
+    <div>
+      <Badge
+        colorScheme={color}
+        fontSize="14px"
+        paddingX={2}
+        borderRadius="4px"
+      >
+        {score}
+      </Badge>
+    </div>
+  );
+};
+
+export default CriticScore;
