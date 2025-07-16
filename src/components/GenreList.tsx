048c86e1f21496cd3cc06eb7d75e47ff0091c2ae
LAPTOP-FH4C51QM\arshi <arshiyataj1412@gmail.com>
2025-07-16 15:06:47 +0530

    Added a spinner for genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,12 @@
-import { HStack, List, Image, Text, Box } from "@chakra-ui/react";
+import { HStack, List, Image, Text, Box, Spinner } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageURl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
   console.log(data);
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
     <>
       <List.Root>
